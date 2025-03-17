47c4d8d75cd20381a41d5a9b8b92b74c76ddb4c6
SimonLee9 <lbj142632@gmail.com>
2025-03-17 17:38:49 +0900

    perf:without login

--- setup_systems_without.sh
+++ setup_systems_without.sh
@@ -176,35 +176,35 @@ echo "========================================"
 
 # 4.1 CMake 3.27.7 (이미 최신 버전이면 skip)
 
-CMAKE_VERSION=3.27.7
-run_step "CMake $CMAKE_VERSION" \
-    "[ -x \$(command -v cmake) ] && cmake --version | grep $CMAKE_VERSION &> /dev/null" \
-    "wget https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION.tar.gz && tar -xvzf cmake-$CMAKE_VERSION.tar.gz && cd cmake-$CMAKE_VERSION && ./bootstrap --qt-gui && make -j$NUM_CORES && sudo make install && cd ~"
-
-# 4.2 Sophus
-run_step "Sophus" \
-    "[ -d Sophus/build ]" \
-    "git clone https://github.com/strasdat/Sophus.git && cd Sophus && mkdir -p build && cd build && cmake .. -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF -DSOPHUS_USE_BASIC_LOGGING=ON && make -j$NUM_CORES && sudo make install && cd ~"
-
-# 4.3 GTSAM (버전 4.2.0)
-run_step "GTSAM" \
-    "[ -d gtsam/build ]" \
-    "git clone https://github.com/borglab/gtsam.git && cd gtsam && git checkout 4.2.0 && mkdir -p build && cd build && cmake .. -DGTSAM_USE_SYSTEM_EIGEN=ON -DGTSAM_BUILD_TESTS=OFF -DGTSAM_BUILD_EXAMPLES_ALWAYS=OFF && make -j$NUM_CORES && sudo make install && cd ~"
-
-# 4.4 OMPL (버전 1.6.0)
-run_step "OMPL" \
-    "[ -d ompl/build ]" \
-    "git clone https://github.com/ompl/ompl.git && cd ompl && git checkout 1.6.0 && mkdir -p build && cd build && cmake .. && make -j$NUM_CORES && sudo make install && cd ~"
-
-# 4.5 socket.io-client-cpp
-run_step "socket.io-client-cpp" \
-    "[ -d socket.io-client-cpp/build ]" \
-    "git clone --recurse-submodules https://github.com/socketio/socket.io-client-cpp.git && cd socket.io-client-cpp && mkdir -p build && cd build && cmake .. -DBUILD_SHARED_LIBS=ON -DLOGGING=OFF && make -j$NUM_CORES && sudo make install && cd ~"
-
-# 4.6 OctoMap (버전 1.10.0)
-run_step "OctoMap" \
-    "[ -d octomap/build ]" \
-    "git clone https://github.com/OctoMap/octomap.git && cd octomap && git checkout v1.10.0 && mkdir -p build && cd build && cmake .. -DBUILD_DYNAMICETD3D=OFF -DBUILD_OCTOVIS_SUBPROJECT=OFF -DBUILD_TESTING=OFF && make -j$NUM_CORES && sudo make install && cd ~"
+#CMAKE_VERSION=3.27.7
+#run_step "CMake $CMAKE_VERSION" \
+#    "[ -x \$(command -v cmake) ] && cmake --version | grep $CMAKE_VERSION &> /dev/null" \
+#    "wget https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION.tar.gz && tar -xvzf cmake-$CMAKE_VERSION.tar.gz && cd cmake-$CMAKE_VERSION && ./bootstrap --qt-gui && make -j$NUM_CORES && sudo make install && cd ~"
+#
+## 4.2 Sophus
+#run_step "Sophus" \
+#    "[ -d Sophus/build ]" \
+#    "git clone https://github.com/strasdat/Sophus.git && cd Sophus && mkdir -p build && cd build && cmake .. -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF -DSOPHUS_USE_BASIC_LOGGING=ON && make -j$NUM_CORES && sudo make install && cd ~"
+#
+## 4.3 GTSAM (버전 4.2.0)
+#run_step "GTSAM" \
+#    "[ -d gtsam/build ]" \
+#    "git clone https://github.com/borglab/gtsam.git && cd gtsam && git checkout 4.2.0 && mkdir -p build && cd build && cmake .. -DGTSAM_USE_SYSTEM_EIGEN=ON -DGTSAM_BUILD_TESTS=OFF -DGTSAM_BUILD_EXAMPLES_ALWAYS=OFF && make -j$NUM_CORES && sudo make install && cd ~"
+#
+## 4.4 OMPL (버전 1.6.0)
+#run_step "OMPL" \
+#    "[ -d ompl/build ]" \
+#    "git clone https://github.com/ompl/ompl.git && cd ompl && git checkout 1.6.0 && mkdir -p build && cd build && cmake .. && make -j$NUM_CORES && sudo make install && cd ~"
+#
+## 4.5 socket.io-client-cpp
+#run_step "socket.io-client-cpp" \
+#    "[ -d socket.io-client-cpp/build ]" \
+#    "git clone --recurse-submodules https://github.com/socketio/socket.io-client-cpp.git && cd socket.io-client-cpp && mkdir -p build && cd build && cmake .. -DBUILD_SHARED_LIBS=ON -DLOGGING=OFF && make -j$NUM_CORES && sudo make install && cd ~"
+#
+## 4.6 OctoMap (버전 1.10.0)
+#run_step "OctoMap" \
+#    "[ -d octomap/build ]" \
+#    "git clone https://github.com/OctoMap/octomap.git && cd octomap && git checkout v1.10.0 && mkdir -p build && cd build && cmake .. -DBUILD_DYNAMICETD3D=OFF -DBUILD_OCTOVIS_SUBPROJECT=OFF -DBUILD_TESTING=OFF && make -j$NUM_CORES && sudo make install && cd ~"
 
 # 4.7 OrbbecSDK (버전 v1.10.11)
 #run_step "OrbbecSDK" \
