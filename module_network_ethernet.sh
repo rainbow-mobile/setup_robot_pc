c8ed732e2421f7e4cd8ac1d919f18f29752f8b7c
SimonLee9 <lbj142632@gmail.com>
2025-03-20 10:17:57 +0900

    feat: network setting - livox

--- module_network_ethernet.sh
+++ module_network_ethernet.sh
@@ -1,32 +1,47 @@
 #!/bin/bash
-# module_network_ethernet.sh: enp2s0 이더넷 네트워크 설정 변경 스크립트
-# IPv4 Method를 Manual로 변경하고, IPv4 주소를 192.168.2.2/24로 설정합니다.
+# module_network_ethernet.sh: 두 이더넷 인터페이스 설정 스크립트
+# enp2s0: IPv4 주소 192.168.2.2/24
+# enp1s0: IPv4 주소 192.168.1.5/24
 
-INTERFACE="enp2s0"
-IPADDR="192.168.2.2"
-PREFIX="24"  # 24는 255.255.255.0 (netmask)에 해당합니다.
+configure_interface() {
+    local interface="$1"
+    local ipaddr="$2"
+    local prefix="$3"
 
-# 활성화된 네트워크 연결 중 enp2s0 인터페이스에 해당하는 연결 이름을 찾습니다.
-CONNECTION_NAME=$(nmcli -t -f NAME,DEVICE connection show --active | grep ":${INTERFACE}$" | cut -d: -f1)
+    echo "[$interface] 인터페이스 설정 중: IP ${ipaddr}/${prefix}"
+    
+    # 활성화된 연결 중 해당 인터페이스에 해당하는 연결 이름 추출
+    local connection_name
+    connection_name=$(nmcli -t -f NAME,DEVICE connection show --active | grep ":${interface}$" | cut -d: -f1)
 
-if [ -z "$CONNECTION_NAME" ]; then
-    echo "[$INTERFACE] 관련 활성화된 네트워크 연결을 찾을 수 없습니다. 수동으로 확인해 주세요."
-    exit 1
-fi
+    if [ -z "$connection_name" ]; then
+        echo "[$interface] 활성화된 연결을 찾을 수 없습니다. 수동으로 확인해 주세요."
+        return 1
+    fi
 
-echo "[$INTERFACE] 연결 이름: $CONNECTION_NAME"
+    echo "[$interface] 연결 이름: $connection_name"
 
-echo "IPv4 Method를 Manual로 변경하고, IPv4 주소를 ${IPADDR}/${PREFIX}로 설정합니다..."
-sudo nmcli connection modify "$CONNECTION_NAME" \
-    ipv4.method manual \
-    ipv4.addresses "${IPADDR}/${PREFIX}" \
-    ipv4.gateway "" \
-    ipv4.dns "" \
-    ipv4.ignore-auto-dns yes
+    # IPv4 설정: Manual 방식, 지정된 IP 주소, 게이트웨이 및 DNS 초기화, 자동 DNS 무시
+    sudo nmcli connection modify "$connection_name" \
+        ipv4.method manual \
+        ipv4.addresses "${ipaddr}/${prefix}" \
+        ipv4.gateway "" \
+        ipv4.dns "" \
+        ipv4.ignore-auto-dns yes
 
-echo "변경사항 적용을 위해 연결을 재시작합니다..."
-sudo nmcli connection down "$CONNECTION_NAME" && sudo nmcli connection up "$CONNECTION_NAME"
+    echo "[$interface] 설정 변경 후 연결 재시작..."
+    sudo nmcli connection down "$connection_name" && sudo nmcli connection up "$connection_name"
 
-echo "현재 IPv4 설정:"
-nmcli -f ipv4.method,ipv4.addresses connection show "$CONNECTION_NAME"
+    echo "[$interface] 현재 IPv4 설정:"
+    nmcli -f ipv4.method,ipv4.addresses connection show "$connection_name"
+    echo "----------------------------------------"
+}
+
+# enp2s0 설정: 192.168.2.2/24
+configure_interface "enp2s0" "192.168.2.2" "24"
+
+# enp1s0 설정: 192.168.1.5/24 (netmask는 enp2s0와 동일하게 /24)
+configure_interface "enp1s0" "192.168.1.5" "24"
+
+echo "두 인터페이스의 네트워크 설정이 완료되었습니다."
 
