3b7548bd13adefb17d78341c43273969a3c37ae0
SimonLee9 <lbj142632@gmail.com>
2025-03-20 14:53:28 +0900

    fix:orbbec -> slamnav2

--- module_orbbec.sh
+++ module_orbbec.sh
@@ -26,7 +26,7 @@ else
     sudo sh -c 'cat >> /etc/profile <<EOF
 export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:/usr/local/lib
 export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:/home/rainbow/rplidar_sdk/output/Linux/Release
-export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:/home/rainbow/OrbbecSDK/SDK/lib
+export LD_LIBRARY_PATH=\${LD_LIBRARY_PATH}:/home/rainbow/slamnav2
 EOF'
 fi
 
