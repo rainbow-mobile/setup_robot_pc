c46867347f80b94120e9a7e1bec8e2d339b0d9dd
SimonLee9 <lbj142632@gmail.com>
2025-04-17 14:16:11 +0900

    dir change

--- setup_sensor.sh
+++ setup_sensor.sh
@@ -4,23 +4,17 @@ set -e
 ###############################################################################
 # install_all.sh
 # - rplidar_sdk, OrbbecSDK, sick_safetyscanners_base 설치
-# - 설치 대상 디렉토리는 항상 /home/원래사용자명 아래로 고정
+# - 설치 대상 경로: /home
 # - 이미 설치된 디렉토리가 있으면 건너뜁니다.
 #
+# 실행 위치: /home/setup_robot_pc
 # 사용법:
 #   sudo ./install_all.sh
 ###############################################################################
 
-# 1) 설치할 홈 디렉토리 결정
-if [ -n "$SUDO_USER" ] && [ "$SUDO_USER" != "root" ]; then
-    USER_NAME="$SUDO_USER"
-else
-    USER_NAME="$(id -un)"
-fi
-HOME_DIR="/home/$USER_NAME"
-echo ">>> 설치 대상 홈 디렉토리: $HOME_DIR (사용자: $USER_NAME)"
+INSTALL_BASE="/home"
+echo ">>> 설치 대상 경로: $INSTALL_BASE"
 
-# 2) 로그 출력 함수
 log() {
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] $*"
 }
@@ -29,13 +23,13 @@ log() {
 # 1. rplidar_sdk 설치
 ########################################
 log "=== [STEP 1] rplidar_sdk 설치 ==="
-if [ -d "$HOME_DIR/rplidar_sdk" ]; then
-    log "[SKIP] $HOME_DIR/rplidar_sdk 이미 존재"
+if [ -d "$INSTALL_BASE/rplidar_sdk" ]; then
+    log "[SKIP] $INSTALL_BASE/rplidar_sdk 이미 존재"
 else
     log "[INSTALL] rplidar_sdk 클론"
-    git clone https://github.com/Slamtec/rplidar_sdk.git "$HOME_DIR/rplidar_sdk"
+    git clone https://github.com/Slamtec/rplidar_sdk.git "$INSTALL_BASE/rplidar_sdk"
     log "[BUILD] rplidar_sdk"
-    cd "$HOME_DIR/rplidar_sdk"
+    cd "$INSTALL_BASE/rplidar_sdk"
     make
     log "[DONE] rplidar_sdk 설치 완료"
 fi
@@ -44,12 +38,12 @@ fi
 # 2. OrbbecSDK 설치
 ########################################
 log "=== [STEP 2] OrbbecSDK 설치 ==="
-if [ -d "$HOME_DIR/OrbbecSDK" ]; then
-    log "[SKIP] $HOME_DIR/OrbbecSDK 이미 존재"
+if [ -d "$INSTALL_BASE/OrbbecSDK" ]; then
+    log "[SKIP] $INSTALL_BASE/OrbbecSDK 이미 존재"
 else
     log "[INSTALL] OrbbecSDK 클론"
-    git clone https://github.com/orbbec/OrbbecSDK.git "$HOME_DIR/OrbbecSDK"
-    cd "$HOME_DIR/OrbbecSDK"
+    git clone https://github.com/orbbec/OrbbecSDK.git "$INSTALL_BASE/OrbbecSDK"
+    cd "$INSTALL_BASE/OrbbecSDK"
     log "[CHECKOUT] v1.10.11"
     git checkout v1.10.11
     log "[UDEV] udev 규칙 설치"
@@ -65,14 +59,14 @@ fi
 # 3. sick_safetyscanners_base 설치
 ########################################
 log "=== [STEP 3] sick_safetyscanners_base 설치 ==="
-if [ -d "$HOME_DIR/sick_safetyscanners_base" ]; then
-    log "[SKIP] $HOME_DIR/sick_safetyscanners_base 이미 존재"
+if [ -d "$INSTALL_BASE/sick_safetyscanners_base" ]; then
+    log "[SKIP] $INSTALL_BASE/sick_safetyscanners_base 이미 존재"
 else
     log "[INSTALL] sick_safetyscanners_base 클론"
-    git clone https://github.com/SICKAG/sick_safetyscanners_base.git "$HOME_DIR/sick_safetyscanners_base"
-    cd "$HOME_DIR/sick_safetyscanners_base"
+    git clone https://github.com/SICKAG/sick_safetyscanners_base.git "$INSTALL_BASE/sick_safetyscanners_base"
+    cd "$INSTALL_BASE/sick_safetyscanners_base"
+    log "[BUILD] 디렉토리 생성 및 CMake"
     mkdir -p build && cd build
-    log "[CMAKE] 구성"
     cmake ..
     log "[MAKE] 병렬 빌드"
     make -j"$(nproc)"
