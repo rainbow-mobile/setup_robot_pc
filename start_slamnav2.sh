2162fd571edd89452b307d67940915c6887ac209
Simon Lee <simonlee9@gmail.com>
2025-08-01 17:39:55 +0900

    update

--- start_slamnav2.sh
+++ start_slamnav2.sh
@@ -7,6 +7,8 @@ export QT_QPA_PLATFORM=xcb
 
 # 라이브러리 경로 설정 (필요시 경로 추가)
 #export LD_LIBRARY_PATH=/usr/local/lib:/opt/opencv/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/rainbow/slamnav2:$LD_LIBRARY_PATH
+
 
 # 실행
 exec ./SLAMNAV2
