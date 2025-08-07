16b83c27db284ffcfb120facf5cd8732121440be
Simon Lee <simonlee9@gmail.com>
2025-08-07 11:28:21 +0900

    fix: xcb

--- start_pimz_slamnav2.sh
+++ start_pimz_slamnav2.sh
@@ -3,12 +3,11 @@ cd /home/odroid/slamnav2
 
 # GUI 환경 보장
 export DISPLAY=:0
+export XAUTHORITY=/home/odroid/.Xauthority
 export QT_QPA_PLATFORM=xcb
 
-# 라이브러리 경로 설정 (필요시 경로 추가)
-#export LD_LIBRARY_PATH=/usr/local/lib:/opt/opencv/lib:$LD_LIBRARY_PATH
+# 라이브러리 경로 설정
 export LD_LIBRARY_PATH=/home/odroid/slamnav2:$LD_LIBRARY_PATH
-
 
 # 실행
 exec ./SLAMNAV2
