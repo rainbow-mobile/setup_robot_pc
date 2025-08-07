0e747ec1ec98112759cacad25b3d32b93713daeb
Simon Lee <simonlee9@gmail.com>
2025-08-07 11:22:31 +0900

    fix : dir

---
+++ start_pimz_slamnav2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd /home/rainbow/slamnav2
+
+# GUI 환경 보장
+export DISPLAY=:0
+export QT_QPA_PLATFORM=xcb
+
+# 라이브러리 경로 설정 (필요시 경로 추가)
+#export LD_LIBRARY_PATH=/usr/local/lib:/opt/opencv/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/odroid/slamnav2:$LD_LIBRARY_PATH
+
+
+# 실행
+exec ./SLAMNAV2
+
